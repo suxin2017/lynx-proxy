121db86cb25e46b1602549b8d4c081f2816467b3
suxin2017 <yishidixiashang@gmail.com>
2025-02-05 13:13:50 +0800

    feat: support tariui (#5)

---
+++ crates/lynx-proxy/mock/__self_service_path__/utils/mockData.ts
@@ -0,0 +1,24 @@
+import Mock from 'mockjs';
+import { mateTypes } from './mockBodyHandle';
+
+export const mockData = (id?: number) => {
+  const uid = id ?? Mock.Random.increment();
+  const type = mateTypes[uid % mateTypes.length];
+  const uri =
+    '@pick(["http","https"])://@domain()/@word()/@word()/' + type.path;
+  return Mock.mock({
+    id: uid,
+    uri:
+      '@pick(["http","https"])://abc.com/ffff/@word(10)/@pick(["a","b","c"])/' +
+      type.path +"?"+ "@word()=" + "@word()",
+    traceId: '@guid',
+    method: '@pick(["GET", "POST", "PUT", "DELETE"])',
+    schema: '@pick(["http", "https"])',
+    version: '@pick(["HTTP/1.1", "HTTP/2"])',
+    statusCode: '@integer(200, 500)',
+    header: {
+      'Content-Type': type.contentType,
+      'User-Agent': '@string("upper", 5, 10)',
+    },
+  });
+};
---
+++ crates/lynx-proxy/src/routes/network/components/Structure/index.tsx
@@ -0,0 +1,52 @@
+import { Splitter } from 'antd';
+import React, { useEffect, useRef } from 'react';
+import { Detail } from '../Detail';
+import { ShowTypeSegmented } from '../ShowTypeSegmented';
+import { RequestTree } from '../RequestTree';
+import { useSize } from 'ahooks';
+
+interface IStructureProps {}
+
+export const Structure: React.FC<IStructureProps> = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const size = useSize(ref);
+  const [sizes, setSizes] = React.useState([240, 400]);
+
+  useEffect(() => {
+    if (size?.width) {
+      setSizes([240, size.width - 240]);
+    }
+  }, [size?.width]);
+
+  console.log(sizes, size);
+
+  return (
+    <div ref={ref} className="h-full bg-white">
+      {size && (
+        <Splitter
+          onResize={(sizes) => {
+            if (sizes[0] < 240) {
+              return;
+            }
+            setSizes(sizes);
+          }}
+          className="h-full bg-white"
+          layout="horizontal"
+        >
+          <Splitter.Panel
+            size={sizes[0]}
+            className="flex flex-col"
+            min="10%"
+            max="70%"
+          >
+            <ShowTypeSegmented />
+            <RequestTree />
+          </Splitter.Panel>
+          <Splitter.Panel size={sizes[1]} min={'40%'} max="90%">
+            <Detail />
+          </Splitter.Panel>
+        </Splitter>
+      )}
+    </div>
+  );
+};
