77fea9ad40c5999afbcc4a6cbf4b8e146a2f085c
suxin2017 <yishidixiashang@gmail.com>
2025-02-10 14:39:34 +0800

    chore: migrate rsbuild to vite for a better developer experience (#17)

--- crates/lynx-proxy/src/routes/setting/index.tsx
+++ crates/lynx-proxy/src/routes/setting/index.tsx
@@ -5,5 +5,5 @@ export const Route = createFileRoute('/setting/')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/home/"!</div>
+  return <div>Hello "/setting/"!</div>
 }
---
+++ crates/lynx-proxy/vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import markdownRawPlugin from 'vite-raw-plugin';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    react(),
+    markdownRawPlugin({
+      fileRegex: /\.md$/,
+    }),
+  ],
+  server: {
+    port: 8080,
+    proxy: {
+      '/__self_service_path__': {
+        target: 'http://127.0.0.1:3000',
+        // onProxyRes(proxyRes, _req, res) {
+        //   res.on('close', () => {
+        //     proxyRes.destroy();
+        //   });
+        //   proxyRes.on('data', () => {
+        //     if (res.closed) {
+        //       proxyRes.destroy();
+        //     }
+        //   });
+        // },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
