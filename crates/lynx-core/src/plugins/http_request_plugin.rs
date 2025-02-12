36964e372c8259d099f1ffa7a2c4804fd2c510ae
suxin2017 <yishidixiashang@gmail.com>
2025-02-12 14:16:11 +0800

    Update release.yml

--- crates/lynx-core/src/plugins/http_request_plugin.rs
+++ crates/lynx-core/src/plugins/http_request_plugin.rs
@@ -201,6 +201,8 @@ pub async fn request(req: Request<Incoming>) -> Result<Response<Incoming>> {
 fn get_test_root_ca(host: Option<&str>) -> hyper_rustls::HttpsConnector<HttpConnector> {
     use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
+    use tokio_rustls::rustls::{ClientConfig, RootCertStore};
+
     fn is_localhost(host: Option<&str>) -> bool {
         match host {
             Some(host) => match host {
