7449834285f52043880053cf2ec7780944d4788e
suxin2017 <yishidixiashang@gmail.com>
2025-04-18 22:01:31 +0800

    done

--- crates/lynx-core/src/proxy/handle_connect_req.rs
+++ crates/lynx-core/src/proxy/handle_connect_req.rs
@@ -2,10 +2,12 @@ use anyhow::{Ok, Result};
 use http::{Request, Response};
 use hyper_util::rt::TokioIo;
 use tokio::spawn;
+use tokio_rustls::TlsAcceptor;
 
 use crate::{
     common::{HyperReq, Res},
     layers::trace_id_layer::service::TraceIdExt,
+    server_context::{CA_MANAGER, get_ca_manager},
     utils::empty,
 };
 
@@ -18,7 +20,11 @@ pub fn is_connect_req<Body>(req: &Request<Body>) -> bool {
 pub async fn handle_connect_req<Body>(req: HyperReq) -> Result<Res> {
     let (part, body) = req.into_parts();
     let extensions = part.extensions.clone();
-
+    let authority = part
+        .uri
+        .authority()
+        .cloned()
+        .ok_or_else(|| anyhow::anyhow!("Missing authority in URI"))?;
     let req = Request::from_parts(part, body);
     let upgraded = hyper::upgrade::on(req).await?;
     let upgraded = TokioIo::new(upgraded);
@@ -33,6 +39,10 @@ pub async fn handle_connect_req<Body>(req: HyperReq) -> Result<Res> {
             // websocket_proxy(req, upgraded).await?;
         }
         ConnectStreamType::Https => {
+            let ca_manager = get_ca_manager();
+            let server_config = ca_manager.get_server_config(&authority).await?;
+            let tls_stream = TlsAcceptor::from(server_config).accept(upgraded).await?;
+
             // Handle HTTP connection
             // proxy_http_request(req, upgraded).await?;
         }
