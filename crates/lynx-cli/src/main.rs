f9644ebe5cc3b4fac810c8094402cf2f5d331a7b
suxin2017 <yishidixiashang@gmail.com>
2025-08-05 10:46:02 +0800

    save code

--- crates/lynx-cli/src/main.rs
+++ crates/lynx-cli/src/main.rs
@@ -4,6 +4,9 @@ use lynx_cli::daemon::DaemonManager;
 use lynx_cli::{Args, Commands, LogConfig, ProxyServerApp, ServerArgs, resolve_data_dir};
 use tokio::signal;
 
+#[global_allocator]
+static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
